ab106eb5c0a0cd4506b543f3312ce2aaa69089ed
liuyanfeng <lyf571321556@gmail.com>
2024-12-12 22:48:27 +0800

    project init

---
+++ pages/demo/demo.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="box" hover-class="boxHover" hover-stay-time="0">
+		view布局标签
+		<view class="innerBox" hover-class="innerHover" hover-stop-propagation hover-stay-time="1">
+			内部元素
+		</view>
+	</view>
+	<text selectable space="ensp">可以复制的文本</text>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.box{
+	width: 200px;
+	height: 200px;
+	background: #ccc;
+}
+
+.boxHover{
+	background: red;
+	width: 200;
+}
+
+.innerBox{
+	width: 150px;
+	height: 150px;
+	background: blue;
+}
+.innerHover{
+	background: purple;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view>你好</view>
+		<view>--**--</view>
+	</view>
+	<view class="layout">
+		<view class="box1">aaa</view>
+		<view class="box2">bbb</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.layout{
+		display: flex;
+		flex-direction: column-reverse;
+		border: 1px solid red;
+		.box1{
+			border: 1px solid blue;
+		}
+		.box2{
+			border: 1px solid green;
+		}
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+	
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+		background-color: aqua;
+	}
+</style>
