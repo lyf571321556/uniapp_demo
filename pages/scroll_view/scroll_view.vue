1a5864214554a6c1cf2e7185d5b4f5ed50a1b7ca
liuyanfeng <lyf571321556@gmail.com>
2024-12-12 23:02:55 +0800

    scroll-view元素的使用

---
+++ pages/scroll_view/scroll_view.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		scroll-view演示
+	</view>
+	<scroll-view scroll-x class="scrollView">
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+		<view class="box">scrollview子元素</view>
+	</scroll-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.scrollView{
+	width: 80%;
+	height: 220px;
+	border:1px solid green;
+	white-space: nowrap; // 不换行显示
+}
+.box{
+	width: 100px;
+	height: 100px;
+	background: green;
+	display: inline-block;	// 行级块元素
+	margin-left: 5px;
+}
+</style>
